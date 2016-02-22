586d6136f77f2a80d1e604f6e319e235161bcd92
Joshua Ide <Josh@192-168-1-100.tpgi.com.au>
2016-02-22 11:08:18 +1100

    Added detail outlets and properties in pokemon class

--- Pokedex/PokemonDetailVC.swift
+++ Pokedex/PokemonDetailVC.swift
@@ -10,14 +10,40 @@ import UIKit
 
 class PokemonDetailVC: UIViewController {
 
+    //Outlets
+    //------------------------
     @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var imgMain: UIImageView!
+    @IBOutlet weak var lblDescription: UILabel!
+    @IBOutlet weak var lblType: UILabel!
+    @IBOutlet weak var lblDefence: UILabel!
+    @IBOutlet weak var lblHeight: UILabel!
+    @IBOutlet weak var lblWeight: UILabel!
+    @IBOutlet weak var lblPokeID: UILabel!
+    @IBOutlet weak var lblBaseAttack: UILabel!
+    @IBOutlet weak var imgCurrentEvo: UIImageView!
+    @IBOutlet weak var imgNextEvo: UIImageView!
+    @IBOutlet weak var lblEvolution: UILabel!
     
+    
+    //Variables
+    //------------------------
     var pokemon: Pokemon!
     
+    
+    //Functions
+    //------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
 
         lblName.text = pokemon.name
+    }
+    
+    
+    //Actions
+    //------------------------
+    @IBAction func onBackPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
     }
 
 
